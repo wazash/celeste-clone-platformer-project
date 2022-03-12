6c998f7ed4106ca3394db203c2e1662df0fdab7a
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-03-12 21:13:56 +0100

    Initial commit

---
+++ 2D Platformer Project/Assets/Scripts/StateMachine/StateBase.cs
@@ -0,0 +1,6 @@
+﻿namespace Assets.Scripts.StateMachine
+{
+    public abstract class StateBase
+    {
+    }
+}
---
+++ 2D Platformer Project/Assets/Scripts/StateMachine/StateMachineBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.StateMachine
+{
+    public abstract class StateMachineBase : MonoBehaviour
+    {
+
+    } 
+}
