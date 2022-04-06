4810819264fa548449a22806cd597aabc21cb4aa
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-04-06 14:02:53 +0200

    Update RoomsVirtualCameraChanging.cs

--- 2D Platformer Project/Assets/Scripts/Rooms/RoomsVirtualCameraChanging.cs
+++ 2D Platformer Project/Assets/Scripts/Rooms/RoomsVirtualCameraChanging.cs
@@ -9,12 +9,14 @@ public class RoomsVirtualCameraChanging : MonoBehaviour
     [SerializeField]
     private CinemachineVirtualCamera virtualCamera;
 
+
     private void OnTriggerEnter2D(Collider2D collision)
-    {   
-        if(collision.CompareTag("Player") && !collision.isTrigger)
+    {
+        if (collision.CompareTag("Player") && !collision.isTrigger)
         {
-            virtualCamera.gameObject.SetActive(true);
-            virtualCamera.enabled = true;
+            //virtualCamera.gameObject.SetActive(true);
+            //virtualCamera.enabled = true;
+            EventsManager.OnCameraPriorityChanged.Invoke(virtualCamera, 11);
         }
     }
 
@@ -22,8 +24,10 @@ public class RoomsVirtualCameraChanging : MonoBehaviour
     {
       if(collision.CompareTag("Player") && !collision.isTrigger)
         {
-            virtualCamera.gameObject.SetActive(false);
-            virtualCamera.enabled = false;
+            //virtualCamera.gameObject.SetActive(false);
+            //virtualCamera.enabled = false;
+
+            EventsManager.OnCameraPriorityChanged.Invoke(virtualCamera, 9);
         }
     }
 }
