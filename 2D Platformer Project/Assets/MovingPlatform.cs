e43994b66d083452c9d1461474250e501fa5df7a
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-04-06 14:05:42 +0200

    Basic moving platform implemented

---
+++ 2D Platformer Project/Assets/MovingPlatform.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField] private float duration;
+
+    [SerializeField] private Transform startPosition, endPosition;
+
+
+    private new Rigidbody2D rigidbody;
+    private Tween tween;
+    private Sequence sequence;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        sequence = DOTween.Sequence();
+        sequence.Append(transform.DOMove(endPosition.position, duration).SetEase(Ease.InCubic));
+        sequence.AppendInterval(1);
+        sequence.Append(transform.DOMove(startPosition.position, duration * 2).SetEase(Ease.Linear));
+        sequence.AppendInterval(.5f);
+        sequence.SetLoops(-1, LoopType.Restart);
+        sequence.OnStepComplete(() => sequence.Pause());
+        sequence.Pause();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.parent = transform;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (!sequence.IsPlaying())
+            {
+                sequence.Play();
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            //sequence.Pause();
+
+            collision.transform.parent = null;
+        }
+    }
+}
