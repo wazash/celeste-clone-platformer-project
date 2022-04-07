1797d87c0c9f9e6337bcde9885c5060c5c793c46
Mateusz Witek <51757549+wazash@users.noreply.github.com>
2022-04-07 16:15:28 +0200

    Gameplay main menu added

--- 2D Platformer Project/Assets/Scripts/MainMenuUI/GameplayMainMenuManager.cs
+++ 2D Platformer Project/Assets/Scripts/MainMenuUI/GameplayMainMenuManager.cs
@@ -4,14 +4,37 @@ public class GameplayMainMenuManager : MonoBehaviour
 {
     [SerializeField] private GameObject menuWindow;
 
-    private void ShowGameplayMainMenu(bool value)
+    private Player player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        ShowGameplayMainMenu(player.InputHandler.PauseInput);
+
+    }
+
+    public void ShowGameplayMainMenu(bool value)
     {
         if (value)
         {
+            player.InputHandler.PauseInput = true;
+            if (player.InputHandler.playerInput.currentActionMap.name != "UI")
+            {
+                player.InputHandler.playerInput.SwitchCurrentActionMap("UI");
+            }
             menuWindow.SetActive(true);
         }
         else
         {
+            player.InputHandler.PauseInput = false;
+            if(player.InputHandler.playerInput.currentActionMap.name != "Gameplay")
+            {
+                player.InputHandler.playerInput.SwitchCurrentActionMap("Gameplay");
+            }
             menuWindow.SetActive(false);
         }
     }
